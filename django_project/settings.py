789309fe55caaa37d9e084a52a79120871c695d7
axel <milad.axel1@gmail.com>
2026-02-19 23:38:15 +0330

    test tanzimat 4

--- django_project/settings.py
+++ django_project/settings.py
@@ -82,11 +82,18 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    "default":  dj_database_url.config(default=config('DATABASE_URL'))
+DATABASES_URL =config('DATABASE_URL')
+if DATABASES_URL :
+    DATABASES = {
+        "default":  dj_database_url.config(default=config('DATABASE_URL', default))
+        }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
